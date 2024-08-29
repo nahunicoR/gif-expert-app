0a127d73e0e2a66ec0384303febd91a44227df84
nahunicoR <nahunico.r@gmail.com>
2024-08-29 11:08:34 -0300

    fix: var

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,8 +1,8 @@
-import { APIKEY } from '../main'
+
 
 export const getGifs = async (category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=L4fAdy0huxBQggHCc1KD1TMM6zBa7Sj2&q=${category}&limit=20`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
