609c698cd6fcd00992b7577344e5d94c226a4d5d
nahunicoR <nahunico.r@gmail.com>
2024-08-29 11:43:42 -0300

    fix APIKEY

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import { GifExpertApp } from './GifExpertApp'
 import "./styles.css"
 
 
-export const APIKEY = 'L4fAdy0huxBQggHCc1KD1TMM6zBa7Sj2';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GifExpertApp />
